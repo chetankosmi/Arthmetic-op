460064f26a254df66056838e89de63205f357414
chetankosmi <chetankosmi24@gmail.com>
2025-10-06 11:49:30 +0530

    Create mathop.py

---
+++ mathop.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform calculations
+sum_result = num1 + num2
+subtraction_result = num1 - num2
+
+# Display results
+print("Sum:", sum_result)
+print("Subtraction:", subtraction_result)
